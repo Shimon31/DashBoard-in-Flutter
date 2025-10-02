0a683dac5bfe7adf0fee3754093f592b4db00df6
shimon <shimonbhuiya02@gmail.com>
2025-10-02 23:52:54 +0600

    Add Search Bar

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -12,7 +12,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[500],
+      backgroundColor: Colors.blue[900],
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(25.0),
@@ -43,15 +43,48 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   ),
                   Container(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue[400],
-                      borderRadius: BorderRadius.circular(8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue[400],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: EdgeInsets.all(12),
+                      child: Icon(Icons.notifications, color: Colors.white),
                     ),
-                    padding: EdgeInsets.all(12),
-                    child: Icon(Icons.notifications, color: Colors.white),
                   ),
+                ],
+              ),
+              SizedBox(height: 20),
+              // search bar
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.blue[600],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.search, color: Colors.white),
+                    SizedBox(width: 5),
+                    Text("Search", style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+              SizedBox(height: 25),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "How Do You Feel Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
                   ),
+                  SizedBox(width: 5,),
+                  Icon(Icons.more_horiz,color: Colors.white,)
                 ],
               ),
             ],
